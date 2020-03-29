aeaf655769dc3bd0053703c54eed13d0d2cd2617
Michael Marcenich <info@m-squared-solutions.it>
2020-03-29 10:59:06 +0200

    fix(routes): remove leading slash on default route

--- routes/nuxt.php
+++ routes/nuxt.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 use M2S\LaravelNuxt\Http\Controllers\NuxtController;
 
 Route::get(
-    '/'.trim(config('nuxt.prefix'), '/').'{path?}',
+    trim(config('nuxt.prefix'), '/').'/{path?}',
     NuxtController::class
 )->name('nuxt')->where('path', '.*');
