40b77fa3bb2a3617e4c553a9119b2b1d2570c26c
Michael Marcenich <info@m-squared-solutions.it>
2020-03-29 10:56:51 +0200

    fix(install): update install command

--- src/Console/Commands/InstallCommand.php
+++ src/Console/Commands/InstallCommand.php
@@ -33,15 +33,21 @@ class InstallCommand extends Command
 
         $this->addArgument(
             'source',
-            InputArgument::REQUIRED,
+            InputArgument::OPTIONAL,
             'Root folder of the nuxt application',
-            resource_path('nuxt')
+            'resources/nuxt'
         );
 
         $this->addOption('yarn', 'y', InputOption::VALUE_NONE, 'Use yarn package manager');
         $this->addOption('typescript', 't', InputOption::VALUE_NONE, 'Use typescript runtime');
         $this->addOption('cache', 'c', InputOption::VALUE_OPTIONAL, 'Optional caching endpoint (e.g. /api/cache)');
         $this->addOption('no-export', null, InputOption::VALUE_NONE, 'Do not export env variable on build');
+        $this->addOption(
+            'prefix',
+            'p',
+            InputOption::VALUE_OPTIONAL,
+            "Prefix for the nuxt application (will use value from `config('nuxt.prefix')` if omitted)"
+        );
     }
 
     /**
@@ -52,69 +58,91 @@ class InstallCommand extends Command
     public function handle()
     {
         $source = base_path($this->argument('source'));
+        $prefixConfig = trim(config('nuxt.prefix'), '/');
+        $yarn = $this->option('yarn') ?: $this->confirm('Use yarn package manager?');
+        $typescript = $this->option('typescript') ?: $this->confirm('Use typescript runtime?');
+        $prefix = trim($this->option('prefix'), '/') ?: $prefixConfig;
 
-        $this->installNuxtRemote($source);
-        $this->installNuxtLocal($source);
-        $this->updatePackageJson();
+        $this->installNuxtRemote($source, $yarn);
+        $this->installNuxtLocal($source, $prefix, $yarn, $typescript);
+        $this->updatePackageJson($typescript);
+
+        $this->output->success("Nuxt integration for source '$source' set up successfully");
+
+        if ($prefixConfig !== $prefix) {
+            $this->output->warning("Set up with custom prefix: '$prefix'
+Current setting: '$prefixConfig'
+Please make sure to publish configuration and to adjust the 'prefix' setting accordingly.");
+            $this->output->write('Publish command: ');
+            $this->comment('php artisan vendor:publish --provider="M2S\\LaravelNuxt\\LaravelNuxtServiceProvider"');
+        }
     }
 
-    protected function installNuxtRemote(string $source)
+    protected function installNuxtRemote(string $source, bool $yarn)
     {
-        if (!file_exists($source) || 2 === count(scandir($source))) {
+        if (!file_exists($source)) {
+            mkdir($source, 0777, true);
+        }
+
+        if (2 === count(scandir($source))) {
             passthru(
-                ($this->option('yarn') ? 'yarn create ' : 'npx create-').'nuxt-app '.$source
+                ($yarn ? 'yarn create ' : 'npx create-').'nuxt-app '.$source
             );
         }
 
         passthru(
             (
-                $this->option('yarn')
+                $yarn
                     ? 'yarn add --dev --cwd'
                     : 'npm i -D --prefix'
             ).
             " $source ".
             'nuxt-laravel@next @nuxtjs/axios'.
-            $this->option('cache') ? ' @nuxtjs/pwa' : ''
+            ($this->option('cache') ? ' @nuxtjs/pwa' : '')
         );
     }
 
-    protected function installNuxtLocal(string $source)
+    protected function installNuxtLocal(string $source, string $prefix, bool $yarn, bool $typescript)
     {
-        $configFile = base_path('nuxt.config.'.($this->option('typescript') ? 'ts' : 'js'));
+        $configFile = base_path('nuxt.config.'.($typescript ? 'ts' : 'js'));
 
         passthru(
             (
-                $this->option('yarn')
+                $yarn
                     ? 'yarn add --dev '
                     : 'npm i -D '
             ).
             'nuxt'.
-            $this->option('typescript') ? ' @nuxt/typescript-runtime' : ''
+            ($typescript ? ' @nuxt/typescript-runtime @nuxt/types' : '')
         );
 
         $config = view(
             'nuxt::config',
             [
-                'source' => $source,
-                'prefix' => trim(config('nuxt.prefix'), '/'),
-                'cache'  => rtrim($this->option('cache'), '/'),
-                'export' => !$this->option('no-export'),
+                'source'     => $source,
+                'prefix'     => $prefix,
+                'typescript' => $typescript,
+                'cache'      => rtrim($this->option('cache'), '/'),
+                'export'     => !$this->option('no-export'),
             ]
         )->render();
 
         file_put_contents($configFile, $config);
     }
 
-    protected function updatePackageJson()
+    protected function updatePackageJson(bool $typescript)
     {
         $packageFile = base_path('package.json');
 
-        $package = json_decode(file_get_contents($packageFile));
-        $nuxt = $this->option('typescript') ? 'nuxt-ts' : 'nuxt';
+        $package = json_decode(file_get_contents($packageFile), true);
+        $nuxt = $typescript ? 'nuxt-ts' : 'nuxt';
 
-        $package->script->{'nuxt:dev'} = $nuxt;
-        $package->script->{'nuxt:build'} = $nuxt.' generate';
-        $package->script->{'nuxt:start'} = $nuxt.' start';
+        if (!isset($package['scripts'])) {
+            $package['scripts'] = [];
+        }
+        $package['scripts']['nuxt:dev'] = $nuxt;
+        $package['scripts']['nuxt:build'] = $nuxt.' generate';
+        $package['scripts']['nuxt:start'] = $nuxt.' start';
 
         file_put_contents($packageFile, json_encode($package, JSON_PRETTY_PRINT));
     }
